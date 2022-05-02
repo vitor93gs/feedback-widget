0d8c24cdfd661a93f18a41e6e721e5e0e36bd55a
Vitor Santos <vitor93gs@gmail.com>
2022-05-02 15:07:19 -0300

    criado hover do widget

--- src/Components/Widget.tsx
+++ src/Components/Widget.tsx
@@ -1,14 +1,18 @@
-interface ButtonProps {
-	text?: string;
-}
+import { useState } from 'react';
 
 import { ChatTeardropDots } from 'phosphor-react';
 
-export function Widget(props: ButtonProps) {
+export function Widget() {
+	const [pressed, setPressed] = useState(false);
 	return (
-		<>
-		<ChatTeardropDots size={300} weight="bold" />
-		</>
-		
+		<div className="absolute bottom-6 right-6">
+			<p >Hello World</p>
+			<button className="bg-red-700 rounded-full px-3 h-12 text-white flex items-center group">
+				<ChatTeardropDots className="w-6 h-6" />
+				<span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-700">
+					Feedback
+				</span>
+			</button>
+		</div>
 	);
 }
