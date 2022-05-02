93b7660e7ea70e1a82a6a5fc58cba515dbd5d2f8
Vitor Santos <vitor93gs@gmail.com>
2022-05-02 14:16:14 -0300

    instalado phosphor-react

--- src/Components/Widget.tsx
+++ src/Components/Widget.tsx
@@ -2,8 +2,13 @@ interface ButtonProps {
 	text?: string;
 }
 
+import { ChatTeardropDots } from 'phosphor-react';
+
 export function Widget(props: ButtonProps) {
 	return (
+		<>
+		<ChatTeardropDots size={300} weight="bold" />
+		</>
 		
 	);
 }
