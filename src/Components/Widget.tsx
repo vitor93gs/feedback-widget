feb3fac269832ec93cfab39dc96fab7ec58dea8c
Vitor Santos <vitor93gs@gmail.com>
2022-05-02 15:30:59 -0300

    criada lógia do popover

--- src/Components/Widget.tsx
+++ src/Components/Widget.tsx
@@ -3,11 +3,19 @@ import { useState } from 'react';
 import { ChatTeardropDots } from 'phosphor-react';
 
 export function Widget() {
-	const [pressed, setPressed] = useState(false);
+	const [pressed, setPressed] = useState(true);
+
+	function togglePressed() {
+		setPressed(!pressed);
+	}
+
 	return (
 		<div className="absolute bottom-6 right-6">
-			<p >Hello World</p>
-			<button className="bg-red-700 rounded-full px-3 h-12 text-white flex items-center group">
+			{!pressed ? <p>Hello World</p> : ""}
+			<button
+				className="bg-red-700 rounded-full px-3 h-12 text-white flex items-center group"
+				onClick={togglePressed}
+			>
 				<ChatTeardropDots className="w-6 h-6" />
 				<span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-700">
 					Feedback
